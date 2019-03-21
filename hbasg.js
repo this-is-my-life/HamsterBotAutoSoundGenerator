08abb7006a3502b239b75e526dd46a2bab602a61
PMH <pmhstudio.pmh@gmail.com>
2019-03-21 13:26:09 +0900

    Update hbasg.js

--- hbasg.js
+++ hbasg.js
@@ -1,108 +1,31 @@
-<!doctype html>
-<html lang="ko" lang="ko-kr">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+const electron = require('electron')
+const app = electron.app
+let mainWindow
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+app.on('ready', () => {
+  createMainScreen()
+})
 
-    <link rel="stylesheet" href="./main.css">
-    <script src="./roboid-1.5.1.js"></script>
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      <div class="container-fluid">
-        <button type="button" class="btn btn-danger" onclick="startProcess(0)">정지</button>
-        <button type="button" class="btn btn-primary" onclick="startProcess(1)">시작</button><hr>
-        <textarea class="form-control" id="noteText" placeholder="이곳에 음을 적으세요"></textarea>
-      </div>
+app.on('activate', () => {
+  if (!mainWindow) {
+    createMainScreen()
+  }
+})
 
-      <script>
-        connect('localhost')
-        const hamster = Hamster.create()
-        function startProcess (ProcessNumber) {
-          if (ProcessNumber === 0) {
-            hamster.wheels(0)
-            hamster.note(0)
-          } else if (ProcessNumber === 1) {
-            let noteText = document.getElementById('noteText').value.split('')
-            console.log('총 계이름 수' + noteText.length)
-            for (let FGcounter = 0; FGcounter < noteText.length; FGcounter++) {
-              setTimeout(() => {
-                hamster.note(0)
-              }, FGcounter * 250 - 20)
-              if (noteText[FGcounter] === '도') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 도')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_C_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '레') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 레')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_D_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '미') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 미')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_E_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '파') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 파')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_F_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '솔') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 솔')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_G_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '라') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 라')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_A_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '시') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 시')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_B_4);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '또') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 또')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_C_5);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '뤠') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 뤠')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_D_5);
-                }, FGcounter * 250)
-              } else if (noteText[FGcounter] === '믜') {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 믜')
-                setTimeout(() => {
-                  hamster.note(Hamster.NOTE_E_5);
-                }, FGcounter * 250)
-              } else {
-                console.log('입력한 계이름 중 ' + FGcounter + '번째 실행됨: 쉼')
-                setTimeout(() => {
-                  hamster.note(0)
-                }, FGcounter * 250)
-              }
-              setTimeout(() => {
-                  hamster.note(0)
-                }, noteText.length * 250)
-            }
-          }
-        }
-      </script>
+function createMainScreen () {
+  mainWindow = new electron.BrowserWindow({
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+  })
+  mainWindow.setMenu(null)
+  mainWindow.toggleDevTools()
+  mainWindow.loadFile('./src/main.html')
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
